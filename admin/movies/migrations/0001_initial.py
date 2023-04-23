d60867dca5b1e810e4739f3f1e001fa48fcd3c64
sbkubric <sbkubric@gmail.com>
2023-04-23 19:40:05 +0300

    fix: FilmWorkGenre -> GenreFilmWork

--- admin/movies/migrations/0001_initial.py
+++ admin/movies/migrations/0001_initial.py
@@ -36,10 +36,7 @@ class Migration(migrations.Migration):
                     "description",
                     models.TextField(blank=True, verbose_name="Description"),
                 ),
-                (
-                    "creation_date",
-                    models.DateField(verbose_name="Creation date"),
-                ),
+                ("creation_date", models.DateField(verbose_name="Creation date")),
                 (
                     "rating",
                     models.FloatField(
@@ -166,7 +163,7 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-            name="FilmWorkGenre",
+            name="GenreFilmWork",
             fields=[
                 (
                     "id",
@@ -193,9 +190,9 @@ class Migration(migrations.Migration):
                 ),
             ],
             options={
-                "verbose_name": "FilmWorkGenre",
-                "verbose_name_plural": "FilmWorkGenres",
-                "db_table": 'content"."film_work_genre',
+                "verbose_name": "GenreFilmWork",
+                "verbose_name_plural": "GenreFilmWork",
+                "db_table": 'content"."genre_film_work',
                 "unique_together": {("genre_id", "film_work_id")},
             },
         ),
@@ -203,7 +200,7 @@ class Migration(migrations.Migration):
             model_name="filmwork",
             name="genres",
             field=models.ManyToManyField(
-                through="movies.FilmWorkGenre", to="movies.Genre"
+                through="movies.GenreFilmWork", to="movies.Genre"
             ),
         ),
     ]
