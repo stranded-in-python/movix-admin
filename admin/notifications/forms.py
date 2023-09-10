25c491c21c3248cfbcd24ff9eacd4b1442272c49
sbkubric <sbkubric@gmail.com>
2023-09-10 18:06:34 +0300

    fix: added cleaning and parsing of rendered html

--- admin/notifications/forms.py
+++ admin/notifications/forms.py
@@ -1,7 +1,10 @@
+from html import parser
+
+import nh3
 from django import forms
 from jinja2 import Environment
 
-from notifications import contexts, models
+from notifications import contexts, enums, models
 
 
 class TemplateAdminForm(forms.ModelForm):
@@ -11,9 +14,14 @@ class TemplateAdminForm(forms.ModelForm):
 
     def clean_body_editable(self):
         template_str = str(self.cleaned_data["body_editable"])
+        mime_type = self.cleaned_data["mime_type"]
         environment = Environment()
         template = environment.from_string(template_str)
         try:
-            template.render(**contexts.CONTEXT_VARS_EXAMPLE)
+            rendered_template = template.render(**contexts.CONTEXT_VARS_EXAMPLE)
+
+            if mime_type == enums.MimeType.TEXT_HTML:
+                rendered_html = nh3.clean(rendered_template)
+                parser.HTMLParser().feed(rendered_html)
         except Exception as e:
             raise forms.ValidationError(str(e))
